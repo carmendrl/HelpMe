327ad0adb33ddd6767b5de85b9db6073269ef063
Andrew Rot <arotc7@gmail.com>
2017-11-11 10:39:04 -0500

    Validate the presence of the text in a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :asker, class_name: "User"
   belongs_to :lab_session
+
+  validates_presence_of :text
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Question do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:text) }
+
+    it { is_expected.to belong_to(:asker) }
+    it { is_expected.to belong_to(:lab_session) }
+  end
+
   it "creates a question" do
     expect do
       create(:question, text: "Why is abbreviated such a long word?")
