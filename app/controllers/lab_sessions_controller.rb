3e757e15c2bd8fa56f2cb66d7171ffbfdb6103e9
Andrew Rot <arotc7@gmail.com>
2017-11-21 20:15:36 -0500

    Standardize errors that are produced
    Generally there will be two different types of errors that can occur. 1
    is that a user cannot find (or access) a record of a certain type. The
    other is that there are improperly formatted or validated inputs. This
    adds error handling for both of these cases in the general case so that
    we can reuse them across all controllers and they are standardized.

--- app/controllers/lab_sessions_controller.rb
+++ app/controllers/lab_sessions_controller.rb
@@ -3,16 +3,9 @@ class LabSessionsController < ApplicationController
 
   def create
     sess = LabSession.new
-    if sess.update(session_params)
+    if sess.update!(session_params)
       render json: sess
     else
-      render json: {
-        status: 422,
-        error: {
-          type: "resource_invalid",
-          errors: serialize_errors(sess),
-        }
-      }
     end
   end
 
@@ -20,14 +13,5 @@ class LabSessionsController < ApplicationController
 
   def session_params
     params.permit(:description, :token, :active)
-  end
-
-  def serialize_errors(object)
-    object.errors.map do |attribute, message|
-      {
-        attribute: attribute,
-        message: message,
-      }
-    end
   end
 end
