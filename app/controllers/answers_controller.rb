b7851118cba1930d0009c937eda2f3f380195fa8
Meredith Lind <meredith@collectiveidea.com>
2017-12-03 14:32:52 -0500

    Small updates to ensure that errors are thrown properly

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,18 +4,18 @@ class AnswersController < ApplicationController
   def show
     sess = current_user.lab_sessions.find(params[:lab_session_id])
     question = sess.questions.find(params[:question_id])
-    render json: question.answer, each_serializer: AnswerSerializer
+    render json: question.answer, serializer: AnswerSerializer
   end
 
   def update
     answer = current_user.lab_sessions.find(params[:lab_session_id]).questions.find(params[:question_id]).answer
-    answer.update(answer_params)
+    answer.update!(answer_params)
 
     render json: answer
   end
 
   def create
-    render json: current_user.answers.create(answer_params)
+    render json: current_user.answers.create!(answer_params)
   end
 
   def destroy
