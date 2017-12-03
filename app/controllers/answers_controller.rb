7188015613e5dffd6a6a034ef25a5a8ede112afb
Meredith Lind <meredith@collectiveidea.com>
2017-12-03 12:15:49 -0500

    Add controller actions

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,33 @@
+class AnswersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    sess = current_user.lab_sessions.find(params[:lab_session_id])
+    question = sess.questions.find(params[:question_id])
+    render json: question.answer, each_serializer: AnswerSerializer
+  end
+
+  def update
+    answer = current_user.answers.find(params[:id])
+    answer.update(answer_params)
+
+    render json: answer
+  end
+
+  def create
+    render json: current_user.answers.create(answer_params)
+  end
+
+  def destroy
+    answer = current_user.lab_sessions.find(params[:lab_session_id]).questions.find(params[:question_id]).answer
+
+    answer.destroy!
+    head :no_content, status: 204
+  end
+
+  private
+
+  def answer_params
+    params.permit(:text, :question_id)
+  end
+end
