b04746c6d0c22e118c8fcad128f7173e631ec7a4
Gabrielle Kosiba <gabrielle.kosiba@hope.edu>
2019-05-20 17:41:48 -0400

    Starting changes to labsession service

--- client/src/app/services/labsession.service.ts
+++ client/src/app/services/labsession.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/of';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Course } from '../models/course.model';
 import { User } from '../models/user.model';
 
@@ -13,16 +14,30 @@ import { ModelFactoryService } from './model-factory.service';
 export class LabSessionService {
 
   private sessions : LabSession[];
+  private apiHost : string;
 
-  constructor(private _modelFactory : ModelFactoryService) {
+  constructor(private httpClient : HttpClient, private _modelFactory : ModelFactoryService) {
 
     this.sessions = [
       _modelFactory.labSession1, _modelFactory.labSession2
     ];
-
+    this.apiHost = host;
   }
 
   get labSessions() : Observable<Array<LabSession>> {
     return Observable.of(this.sessions);
+
+    //Start added interaction with server code
+        let url : string =`${this.apiHost}/lab_sessions/`;
+        //all below needs to be changed (jjust copied from user.service)
+
+        return this.httpClient.post<UserResponseData>(url, body).pipe(
+
+          tap(r => this.updateLoggedInUserFromResponse(new UserResponse(r["data"]))),
+          map(r => {
+            return true
+          }),
+          catchError(error => this.handleError(error))
+        );
   }
 }
