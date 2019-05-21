eb784e4ef7083f9624434e212ff33dc638aae631
Gabrielle Kosiba <gabrielle.kosiba@hope.edu>
2019-05-21 16:09:31 -0400

    Labsession Variables implemented and Ids changed to number type

--- client/src/app/services/labsession.service.ts
+++ client/src/app/services/labsession.service.ts
@@ -6,6 +6,8 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { API_SERVER } from '../app.config';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 import { LabSession } from '../models/lab_session.model';
+import { Course } from '../models/course.model';
+import { User } from '../models/user.model';
 import { map, catchError, tap, delay, timeout } from 'rxjs/operators';
 import { ModelFactoryService } from './model-factory.service';
 import { of } from 'rxjs/observable/of';
@@ -14,9 +16,9 @@ class LabsessionResponseAttributes {
   public description : string;
   public token : string;
   public activeStatus : boolean;
-  public course_id : string;
-  public start_date:string;
-  public end_date:string;
+  public course_id : number;
+  public start_date: Date;
+  public end_date: Date;
 }
 
 class LabsessionResponseRelationships{
@@ -34,7 +36,7 @@ class LabsessionResponseUsers {
 }
 
 class LabsessionResponseUsersData {
-  public id : string;
+  public id : number;
   public type: string;
 }
 
@@ -43,14 +45,15 @@ class LabsessionResponseCourse {
 }
 
 class LabsessionResponseCourseData {
-  public id: string;
+  public id: number;
   public type: string;
 }
 
 
 class LabsessionResponseData {
+  constructor(private)
   public type : string;
-  public id : string;
+  public id : number;
   public attributes: LabsessionResponseAttributes;
   public relationships : LabsessionResponseRelationships;
 }
@@ -59,27 +62,27 @@ class LabsessionResponse {
   constructor (private data : LabsessionResponseData) {
 	}
   get Type() : string { return this.data.type }
-  get Id() : string { return this.data.id }
+  get Id() : number { return this.data.id }
   get Description() : string { return this.data.attributes["description"] }
   get Token() : string { return this.data.attributes["token"] }
   get ActiveStatus() : boolean { return this.data.attributes["active"] }
-  get StartDate() : string { return this.data.attributes["start-date"]}
-  get EndDate() : string { return this.data.attributes["end-date"]}
-  get CourseId() : string { return this.data.attributes["course-id"]}
+  get StartDate() : Date { return this.data.attributes["start-date"]}
+  get EndDate() : Date { return this.data.attributes["end-date"]}
+  get CourseId() : number { return this.data.attributes["course-id"]}
   get Rdata() : string[] { return this.data.relationships.questions["data"]}
-  get userId() : string {return this.data.relationships.users.data["id"]}
+  get userId() : number {return this.data.relationships.users.data["id"]}
   get userType() : string { return this.data.relationships.users.data["type"]}
 }
 
 class IncludedCourseObjResponse{
-  constructor (private data: IncludedObjResponseData )
+  constructor (private data: IncludedCourseObjResponseData){}
 }
 
 class IncludedCourseObjResponseData{
   public type : string;
   public id : string;
-  public attributes: ;
-  public relationships : ;
+  //public attributes: ;
+  //public relationships : ;
 
 }
 
@@ -102,7 +105,7 @@ export class LabSessionService {
   labSessions() : Observable<LabSession[]> {
         let url : string =`${this.apiHost}/lab_sessions`;
         return this.httpClient.get(url).pipe(
-          map(r => this.createLabsessionsArray(r["data"])),
+          map(r => this.createLabsessionsArray(new Array<LabsessionResponse>(r["data"]))),
           catchError(this.handleError<LabSession[]>(`labSessions`))
         );
         //return this._currentSessions$;
@@ -118,21 +121,23 @@ export class LabSessionService {
   //   );
   // }
   //
-  private createLabsessionsArray(object: Object[]) : LabSession[]{
+  private createLabsessionsArray(objects: LabsessionResponse[]) : LabSession[]{
     let sessions = new Array<LabSession>();
-     for(let obj in object){
-       sessions.push(this.buildCreateLabsessionFromJson(obj));
+     for(let obj in objects){
+       sessions.push(this.buildCreateLabsessionFromJson(new LabsessionResponse(obj)));
      }
     return sessions;
   }
 
     private buildCreateLabsessionFromJson(s: LabsessionResponse ) : LabSession {
-        let session = new LabSession();
-        session.description = s.Description;
-        session.start_date = s.StartDate;
-        session.end_date = s.EndDate;
-        session.course = (new Course("CSCI","150","Web Design and Implementation","201801",(new User("professorlogin@test.com",
-        "ryanmcfall-prof","Ryan", "McFall","professors","cd11850c-4dbb-4e71-a6c3-e14ec69847ae","password"))));
+        let session = new LabSession(s.Description, s.StartDate, s.EndDate, new Course("CSCI","150","Web Design and Implementation","201801",(new User("professorlogin@test.com",
+        "ryanmcfall-prof","Ryan", "McFall"))), s.Id);
+        // session.description = s.Description;
+        // session.start_date = s.StartDate;
+        // session.end_date = s.EndDate;
+        // session.id = s.Id
+        // session.course = (new Course("CSCI","150","Web Design and Implementation","201801",(new User("professorlogin@test.com",
+        // "ryanmcfall-prof","Ryan", "McFall","professors","cd11850c-4dbb-4e71-a6c3-e14ec69847ae","password"))));
         return session;
     }
   //
