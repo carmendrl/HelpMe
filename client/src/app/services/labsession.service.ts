0f052e48bf9d3a83e38cc02b7f7897d4864e5a0c
Claire Lundy <claire.lundy@hope.edu>
2019-05-21 10:53:37 -0400

    added createLabsessions and other complimentary functions.

--- client/src/app/services/labsession.service.ts
+++ client/src/app/services/labsession.service.ts
@@ -79,32 +79,38 @@ export class LabSessionService {
   get CurrentSessions$() : Observable<Labsessions> {
     return this._currentSession$;
   }
-  
+
   get labSessions() : Observable<LabSession[]> {
         let url : string =`${this.apiHost}/lab_sessions/`;
 
-<<<<<<< HEAD
-        return this.httpClient.post<UserResponseData>(url).pipe(
-          //timeout(5000), //possible other way to have login delay messsage possibly displayed.
-          //delay(20000), //This is here to test for login delay messages
-          tap(r => this.updateLabsessionsFromResponse(new LabsessionResponse(r["data"]))),
-          map(r => {
-            return true
-          }),
-=======
         return this.httpClient.post<LabsessionResponseData>(url, body).pipe(
 
           map(r => (this.updateLabsessionsFromResponse(new LabsessionResponse(r["data"])))),
->>>>>>> 372ecf6372a7b59a711309f087a45f412065d1ad
           catchError(error => this.handleError(error))
         );
   }
+
+  createLabsessions(session : Labsession) : Observable<boolean> {
+    let url : string = `${this.apiHost}/lab_sessions`;
+    let body = this.buildCreateLabsessionBodyFromSession (session);
+    return this.httpClient.post<UserResponseData>(url, body).pipe(
+      tap(r => this.updateLabsessionsFromResponse(new LabsessionResponse(r["data"]))),
+      map(r => true ),
+      catchError(error => this.handleCreateAccountError(error))
+    );
+  }
+
+    private buildCreateLabsessionBodyFromSession ( s : Labsession ) {
+      return {
+        description : s.description,
+        course_id : s.course_id
+      };
+    }
 
     private updateLabsessionsFromResponse(r : LabsessionResponse) {
         let session = new Labsession();
         session.description = r.description;
         session.id = r.id;
-<<<<<<< HEAD
         this._currentSessions$.next(session);
 
     }
@@ -118,9 +124,6 @@ export class LabSessionService {
       }
       return of(false);
     }
-=======
-        
->>>>>>> 372ecf6372a7b59a711309f087a45f412065d1ad
 
     private handleError (error) : Observable<boolean> {
       return of(false);
