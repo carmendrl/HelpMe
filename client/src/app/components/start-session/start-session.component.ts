6973605532f3e3009ffe9f68f73f5cc52ab976de
Claire Lundy <claire.lundy@hope.edu>
2019-05-23 10:32:55 -0400

    Created modal. Also updated the version of TS to ~3.2.2. You must install the version by using npm install in the console.

--- client/src/app/components/start-session/start-session.component.ts
+++ client/src/app/components/start-session/start-session.component.ts
@@ -1,33 +1,36 @@
-import { Component, OnInit } from '@angular/core
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-start-session',
   templateUrl: './start-session.component.html',
   styleUrls: ['./start-session.component.scss']
 })
-export class StartSessionComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+export class StartSessionComponent implements OnInit {
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit() {
   }
 
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(content, <NgbModalOptions>{ariaLabelledBy: 'modal-create-session'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
 
+
   private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
+     if (reason === ModalDismissReasons.ESC) {
+       return 'by pressing ESC';
+     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+       return 'by clicking on a backdrop';
+     } else {
+       return  `with: ${reason}`;
+     }
+   }
 }
