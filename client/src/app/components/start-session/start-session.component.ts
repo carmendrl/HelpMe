c5c6413e07a9875fb06700e3817d125f0b544970
Gabrielle Kosiba <gabrielle.kosiba@hope.edu>
2019-05-29 10:45:14 -0400

    Start Session edited to condense code and allow sessions to be created when a session is added

--- client/src/app/components/start-session/start-session.component.ts
+++ client/src/app/components/start-session/start-session.component.ts
@@ -6,7 +6,6 @@ import {Course} from '../../models/course.model';
 import { LabSession } from '../../models/lab_session.model';
 import { LabSessionService } from '../../services/labsession.service';
 import { CourseService } from '../../services/course.service';
-import {NgbModal, ModalDismissReasons, NgbModalOptions, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -30,96 +29,63 @@ export class StartSessionComponent implements OnInit {
   private newCourse: Course;
 
   private todayYear: number;
-  //private coursesPresent: boolean;
 
   private selectedCourse : Course;
-  private addedCourse = false;
 
 
 
   constructor( @Inject(DOCUMENT) public document: Document,
-  private router : Router, private labSessionService: LabSessionService, private courseService: CourseService, private modalService: NgbModal){
+  private router : Router, private labSessionService: LabSessionService, private courseService: CourseService){
   }
 
   ngOnInit() {
     this.sessionStarted = false;
-    debugger
     this.courseService.coursesList().subscribe(
-      courses => this.startCourse = courses);
-      //this.numOfCourses();
-      debugger
-    this.courseService.newCourse$.subscribe(c => {this.startCourse.unshift(c);this.selectedCourse=c;});
-  }
-
-
-
-
+      courses => {this.startCourse = courses; if (courses.length> 0){this.selectedCourse = this.startCourse[0]}});
+      this.courseService.newCourse$.subscribe(c => {this.newCourse = c; this.startCourse.unshift(c)});
+    }
 
     startSession(){
       debugger
-      this.labSessionService.createNewLabSession(this.description, this.selectedCourse.id).subscribe(
-      r => {this.newSession = r; this.generatedId= this.newSession.id; this.generatedCode= this.newSession.token});
-      this.sessionStarted = true;
-   }
+      this.courseId = this.selectedCourse.id;
+      this.labSessionService.createNewLabSession(this.description, this.courseId).subscribe(
+        r => {this.newSession = r; this.generatedId= this.newSession.id; this.generatedCode= this.newSession.token});
+        debugger
+        this.sessionStarted = true;
+      }
 
+      copySessionCode(){
 
+        let selBox = this.document.createElement('textarea');
+        selBox.value=this.generatedCode;
+        this.document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        this.document.execCommand('copy');
+        this.document.body.removeChild(selBox);
 
-  copySessionCode(){
+      }
+      copySessionLink(){
 
-    let selBox = this.document.createElement('textarea');
-    selBox.value=this.generatedCode;
-    this.document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    this.document.execCommand('copy');
-    this.document.body.removeChild(selBox);
+        let selBox = this.document.createElement('textarea');
+        let url ="www.YouDidIT....."+this.generatedId+".....com";
+        selBox.value=url; ///////NEED TO CHANGE THIS TO URL TO GO TO SESSION
+        this.document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
+        this.document.execCommand('copy');
+        this.document.body.removeChild(selBox);
 
-  }
-  copySessionLink(){
-
-    let selBox = this.document.createElement('textarea');
-    let url ="www.YouDidIT....."+this.generatedId+".....com";
-    selBox.value=url; ///////NEED TO CHANGE THIS TO URL TO GO TO SESSION
-    this.document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    this.document.execCommand('copy');
-    this.document.body.removeChild(selBox);
-
-  }
-
-  open(content) {
-    //this.sessionStarted =false;
-    let modal= this.modalService.open(content, <NgbModalOptions>{ariaLabelledBy: 'modal-create-course'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-    console.log("Testing Modal");
-    //this.courseService.newCourse$.subscribe(c => modal.close(c));
-  }
-
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
-
+      }
 
 
       saveCourse(){
         this.courseService.newCourse$.subscribe(c => {this.newCourse = c; this.startCourse.unshift(c)});
-        this.addedCourse = true;
       }
-      // createNewCourseFromForm(){
-      //   debugger
-      //   let yearSemester = this.todayYear + this.semester;
-      //   this.courseService.postNewCourse(this.subject, this.number, this.title, yearSemester).subscribe(
-      //     r => this.startCourse.unshift(r));
-      //   }
+      createNewCourseFromForm(){
+        debugger
+        let yearSemester = this.todayYear + this.semester;
+        this.courseService.postNewCourse(this.subject, this.number, this.title, yearSemester).subscribe(
+          r => this.startCourse.unshift(r));
+        }
       }
