322f8d24b07af0992550468d42421023f8164941
Ryan McFall <mcfall@hope.edu>
2019-06-11 18:33:02 -0400

    Set up CORS configuration for web API

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,15 @@ module BackendApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    #  Configuration for CORS requests.  Allow any GET, POST, OPTIONS requests
+    #  from any client
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options, :patch, :put]
+      end
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
