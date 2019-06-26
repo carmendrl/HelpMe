9b45763e14b5daba7485b074c300535433645b45
Claire Lundy <claire.lundy@hope.edu>
2019-06-26 17:40:00 -0400

    working on ta view tests

--- client/e2e/taViewTests.e2e-spec.ts
+++ client/e2e/taViewTests.e2e-spec.ts
@@ -27,9 +27,70 @@ describe('ta login and act as student', () => {
 
     //verify that it is the correct dashboard
     expect(page.getStudentDashboard()).toBeTruthy();
-
+    page.getProfileMenu().click();
     page.getSwitchBackButton().click();
     expect(page.getFacultyDashboard()).toBeTruthy();
     expect(page.getJoinBox()).toBeTruthy();
+  });
+
+  it('ta can create sessions', () =>{
+    page.navigateTo2();
+    let a = page.getSessionsLength().then((i:number) => {return i+1});
+    page.setUpSession();
+    page.startSession().click();
+    //browser.sleep(6000);
+    page.navigateTo2();
+    expect(page.getSessionsLength()).toBe(a);
+  });
+
+  it('ta can claim a question', () =>{
+    page.navigateTo2();
+    page.getSessionList();
+    //page.getSession();
+    page.getViewButton().click();
+    //browser.sleep(6000);
+    let a = page.getMyQuestionsLength().then((i:number) => {return i+1});
+    let b = page.getUnclaimedQuestionsLength().then((i:number) => {return i-1});
+    page.getUnclaimedOpen();
+    page.claim().click();
+    page.navigateTo2();
+    page.getSessionList();
+    //page.getSession();
+    page.getViewButton().click();
+    expect(page.getMyQuestionsLength()).toBe(a);
+    expect(page.getUnclaimedQuestionsLength()).toBe(b);
+  });
+  it('ta can unclaim a question', () =>{
+    page.navigateTo2();
+    page.getSessionList();
+    //page.getSession();
+    page.getViewButton().click();
+    let a = page.getMyQuestionsLength().then((i:number) => {return i-1});
+    let b = page.getUnclaimedQuestionsLength().then((i:number) => {return i+1});
+    page.getMyOpen().click();
+    page.unclaim().click();
+    page.navigateTo2();
+    page.getSessionList();
+    //page.getSession();
+    page.getViewButton().click();
+    expect(page.getMyQuestionsLength()).toBe(a);
+    expect(page.getUnclaimedQuestionsLength()).toBe(b);
+    });
+
+    it('ta can add to faq', () => {
+      page.getFaQlist();
+      page.getFaqOpen().click();
+      page.faq().click();
+    });
+  //logout
+  it('should open profile menu',() =>{
+    page.navigateTo();
+    expect(page.getProfileMenuComponent()).toBeTruthy();
+    expect(page.getProfileMenu()).toBeTruthy();
+  });
+
+  it('should log out', () =>{
+    page.getProfileMenu().click();
+    page.getLogoutButton().click();
   });
 });
--- client/e2e/taViewTests.po.ts
+++ client/e2e/taViewTests.po.ts
@@ -4,6 +4,9 @@ export class TaViewTests{
   navigateTo(){
     browser.get('/login');
   }
+  navigateTo2(){
+    browser.get('/dashboard');
+  }
   getEmailTextbox() {
     return element(by.name('Email'));
   }
@@ -33,11 +36,119 @@ export class TaViewTests{
     return this.getProfileMenu().element(by.name('taSwitch'));
   }
   getSwitchBackButton(){
-    this.getProfileMenu().click();
-    return element(by.name('taSwitchBack'));
+    return this.getProfileMenu().element(by.name('taSwitchBack'));
   }
 
   getLogoutButton(){
     return element(by.css('.logout'));
+  }
+
+  getStartSessionComponent(){
+    return element(by.tagName('app-start-session'));
+  }
+  getForm(){
+    return element(by.css('.startSessionForm'));
+  }
+
+  getDescriptionTextBox(){
+    return element(by.name('description'));
+  }
+
+  getStartDateTextBox(){
+    return element(by.name('dp'));
+  }
+
+  getStartTimePickerHour(){
+    return element(by.name('start_time')).element(by.css('.ngb-tp-hour')).element(by.tagName('input'));
+  }
+
+  getStartTimePickerMinute(){
+    return element(by.name('start_time')).element(by.css('.ngb-tp-minute')).element(by.tagName('input'));
+  }
+
+  getEndDateTextBox(){
+    return element(by.name('end_date'));
+  }
+
+  getEndTimePickerHour(){
+    return element(by.name('end_time')).element(by.css('.ngb-tp-hour')).element(by.tagName('input'));
+  }
+
+  getEndTimePickerMinute(){
+    return element(by.name('end_time')).element(by.css('.ngb-tp-minute')).element(by.tagName('input'));
+  }
+
+  getPMButton(){
+    return element(by.name('end_time')).element(by.css('.btn-outline-primary'));
+  }
+  setUpSession(){
+    this.getStartSessionComponent();
+    this.getForm();
+    this.getDescriptionTextBox().sendKeys('test session');
+    this.getStartDateTextBox().sendKeys('2019-06-26');
+    this.getStartTimePickerHour().sendKeys('10');
+    this.getStartTimePickerMinute().sendKeys('00');
+    this.getEndDateTextBox().sendKeys('2019-06-26');
+    this.getEndTimePickerHour().sendKeys('05');
+    this.getEndTimePickerMinute().sendKeys('30');
+    this.getPMButton().click();
+  }
+  startSession(){
+    return element(by.name('start'));
+  }
+  getSessionList(){
+    return element(by.tagName('app-session-list'));
+  }
+  getViewButton(){
+    return this.getSessionList().element(by.name('view'));
+  }
+  claim(){
+    return element(by.name('claim'));
+  }
+  unclaim(){
+    //this.getUnclaimedQuestions();
+    return element(by.name('unclaim'));
+  }
+  faq(){
+    return element(by.name('addFaq'))
+  }
+  getUnclaimedQuestions(){
+    this.getFacultySessionView();
+    return element(by.id('unclaimedQs'));
+  }
+  getFaQlist(){
+    this.getFacultySessionView();
+    return element(by.id('faqs'));
+  }
+  getMyQuestions(){
+    this.getFacultySessionView();
+    return element(by.id('claimedQs'));
+  }
+  getUnclaimedQuestionsLength(){
+    return this.getUnclaimedQuestions().all(by.id('questionRow')).count();
+  }
+  getMyQuestionsLength(){
+    this.getFacultySessionView();
+    return this.getMyQuestions().all(by.id('questionRow')).count();
+  }
+  getFaqLength(){
+    this.getFacultySessionView();
+    return this.getFaQlist().all(by.id('questionRow')).count();
+  }
+  getFacultySessionView(){
+    return element(by.tagName('app-faculty-session-view'));
+  }
+  getSessionsLength(){
+    this.getSessionList();
+    return this.getSessionList().all(by.id('tableRow')).count();
+  }
+  getUnclaimedOpen(){
+    return this.getUnclaimedQuestions().element(by.name('open'));
+  }
+  getMyOpen(){
+    return this.getMyQuestions().element(by.name('open'));
+  }
+  getFaqOpen(){
+    return this.getFaQlist().element(by.name('open'));
   }
 }
