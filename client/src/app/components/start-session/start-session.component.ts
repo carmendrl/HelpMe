2afa8e6d8115d85083957fc10bb6ae4d0ee48d19
Gabrielle Kosiba <gabrielle.kosiba@hope.edu>
2019-07-03 09:49:31 -0400

    "Go to session" link goes to session now (is not undefined!!)

--- client/src/app/components/start-session/start-session.component.ts
+++ client/src/app/components/start-session/start-session.component.ts
@@ -24,6 +24,7 @@ export class StartSessionComponent implements OnInit {
   private year: string;
   private startCourse : Course[];
   private generatedCode: string;
+  private generatedId:string;
   private newSessionDescription:string;
   private newSession: ApiResponse<LabSession>;
   private newCourse: Course;
@@ -85,7 +86,7 @@ export class StartSessionComponent implements OnInit {
     this.compareStartEnd();
       if(this.startBeforeEnd){
     this.labSessionService.createNewLabSession(this.description, this.selectedCourse.id, this.fullStartDate, this.fullEndDate).subscribe(
-      r => {this.newSession = r; this.generatedCode=this.newSession.Data.token; this.newSessionDescription=this.newSession.Data.description; this.handleCreateSessionError(r)});
+      r => {this.newSession = r; this.generatedCode=this.newSession.Data.token; this.generatedId=this.newSession.Data.id; this.newSessionDescription=this.newSession.Data.description; this.handleCreateSessionError(r)});
     }
     }
 
@@ -104,7 +105,6 @@ export class StartSessionComponent implements OnInit {
     }
       createStartEnd(){
         this.start_date;
-        debugger
         this.fullStartDate = this.start_date.year +"-"+ this.start_date.month  +"-"+ this.start_date.day +"T"+ (this.start_time.hour+ 4) +":"+
          this.start_time.minute +":"+ this.start_time.second +"Z";
          this.fullEndDate = this.end_date.year +"-"+ this.end_date.month  +"-"+ this.end_date.day +"T"+ (this.end_time.hour+4) +":"+
@@ -139,7 +139,7 @@ export class StartSessionComponent implements OnInit {
     copySessionCode(){
       this.copied = true;
       let selBox = this.document.createElement('textarea');
-      selBox.value=this.newSession.Data.token;
+      selBox.value=this.generatedCode;
       this.document.body.appendChild(selBox);
       selBox.focus();
       selBox.select();
@@ -150,7 +150,7 @@ export class StartSessionComponent implements OnInit {
     copySessionLink(){
       this.copied = true;
       let selBox = this.document.createElement('textarea');
-      let url =`${environment.server}/lab_sessions/${this.newSession.Data.id}`;
+      let url =`${environment.server}/lab_sessions/${this.generatedId}`;
       selBox.value=url;
       this.document.body.appendChild(selBox);
       selBox.focus();
